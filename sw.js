e2bd99fd950e3342c84d316e07965f3871711051
iBeePhoto <ibeephoto87@gmail.com>
2026-06-27 16:45:28 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'din-esan-v1';
+const CACHE_NAME = 'din-esan-v3';
 const APP_FILES = ['./', './index.html', './style.css', './logo.png', './site.webmanifest'];
 
 self.addEventListener('install', (event) => {
